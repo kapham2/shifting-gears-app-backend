8795c504b23b6f34862dd2c34cf8d8a26584b804
Kim Pham <pham.kim.a@gmail.com>
2019-03-05 22:31:04 -0600

    change 'login' to 'signin'

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AuthController < ApplicationController
-    skip_before_action :authorized, only: [:login]
+    skip_before_action :authorized, only: [:signin]
 
     def signin
         @user = User.find_by(username: signin_params[:username])
